e2252be2aa489c07a138597352787619d3f07594
Juan Aristizabal <spjhon@gmail.com>
2023-11-23 14:39:23 -0500

    adelanto

--- 003ReactDocs-Example2-Search/README.md
+++ 003ReactDocs-Example2-Search/README.md
@@ -11,7 +11,7 @@ Temas Tratados en este Ejercicio
 - Renderizado bajo condiciones de acuerdo a como vengan los datos desde el json o un objetc
 - Curioso pattern en donde se muestra como filtrar informacion y renderizar solo lo que se desea en una lista
 - Un curioso truco en donde el metodo indexOf retorna 0 cuando se ejecuta sobre una palabra y no un array.
-- Utilizacion de propTypes por parte de una deteccion de esLint.
+- Utilizacion de propTypes por parte de una deteccion de esLint (deprecated, usar TYPESCRIPT).
 
 Hooks Utilizados
 
--- 003ReactDocs-Example2-Search/src/components/SearchBar.jsx
+++ 003ReactDocs-Example2-Search/src/components/SearchBar.jsx
@@ -1,11 +1,28 @@
+import PropTypes from "prop-types";
+
 export default function SearchBar({
   filterText,
   inStockOnly,
   onFilterTextChange,
   onInStockOnlyChange,
 }) {
+  SearchBar.propTypes = {
+    products: PropTypes.object.isRequired,
+    filterText: PropTypes.string.isRequired,
+    inStockOnly: PropTypes.bool.isRequired,
+
+    onFilterTextChange: PropTypes.func.isRequired,
+    onInStockOnlyChange: PropTypes.func.isRequired,
+  };
 
   //Aqui se muestra como el set se forma al traer el setState desde arriba por medio de props
+
+  //En este paso, resulta que si se coloca un input o cualquier cosa que cambie de estado por el usuario ocualquier otra cosa,
+  //hay que avisarle al responsable de controlar ese estado para que actuelice el state, esto es por diseño de react para que no
+  //hayan cambios de states sin rastrear.
+
+  //El onChange es parte de la tabla de eventos HTML, segun veo se aplica los atributos de eventos html directamente en el JSX.
+
   return (
     <form>
       <input
@@ -19,7 +36,8 @@ export default function SearchBar({
           type="checkbox"
           checked={inStockOnly}
           onChange={(e) => onInStockOnlyChange(e.target.checked)}
-        /> Only show products in stock
+        />{" "}
+        Only show products in stock
       </label>
     </form>
   );
